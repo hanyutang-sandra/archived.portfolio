d26fa7f7003e1ee7af7ffc1f122385997d7ff8ab
Hanyu <hanyutang.sandra@gmail.com>
2019-04-11 23:13:09 -0400

    Dummy pics

--- js/about.js
+++ js/about.js
@@ -3,21 +3,18 @@ $(document).ready(function(){
         $('body').removeClass('fade-out');
     });
 
-    let height = $('.img').height();
-    $('.background').css({height: height});
-
     setTimeout(headerEnter(), 300);
 
     function headerEnter(){
         anime.timeline({loop: false})
             .add({
-                targets: '.background',
-                width: '100%',
+                targets: '.photo',
+                opacity: '1',
                 easing: 'easeInOutQuad',
                 direction: 'alternate',
             })
             .add({
-                targets: '.photo',
+                targets: '.text',
                 opacity: '1',
                 easing: 'easeInOutQuad',
                 direction: 'alternate',
